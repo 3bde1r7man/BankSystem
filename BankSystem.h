9a493bfd4877adb87a2555f7f5393e5bbd7d132a
Abdelrhman Mostafa <abdelrhman.m.hessain@gmail.com>
2022-11-04 01:42:47 +0200

    test

--- BankSystem.h
+++ BankSystem.h
@@ -7,76 +7,6 @@
 
 using namespace std;
 
-
-class BankApp
-{
-private:
-	vector<BankAcc> bankAcc;
-public:
-	void menu();
-	void addClient();
-	void List();
-	void withdraw();
-	void deposit();
-	BankApp();
-	virtual ~BankApp() {
-
-	}
-};
-
-class BankAcc
-{
-
-protected:
-	string accID;
-	Client* client;
-	double balance;
-	bool accType;
-
-public:
-	//___________________________________BankAcc________________________________
-	BankAcc(Client& client);
-	virtual ~BankAcc() {
-
-	}
-	void setAccID(string id);
-	void setBalance(double bal);
-	void setAccType(bool type);
-	string getAccID();
-	double getBalance();
-	bool getAccType();
-	Client* get_client();
-	virtual int withdraw(double amount);
-	virtual int deposit(double amount);
-	bool isFoundAccID(string id);
-
-
-
-
-};
-
-class SavingBankAcc :public BankAcc
-{
-private:
-	double minimumBalance, minimumWithdraw;
-public:
-	SavingBankAcc(Client& client) : BankAcc(client) {
-		minimumBalance = 1000.0;
-		minimumWithdraw = 100.0;
-	}
-	int withdraw(double amount);
-	int deposit(double amout);
-	virtual ~SavingBankAcc() {
-
-	}
-	int getMiniBalace() {
-		return minimumBalance;
-	}
-	int getMIniWithdraw() {
-		return minimumWithdraw;
-	}
-};
-
 class Client
 {
 private:
@@ -98,3 +28,72 @@ public:
 	bool isValidPhoneNumber(string p);
 };
 
+
+class BankAcc
+{
+
+protected:
+	string accID;
+	Client* client;
+	double balance;
+	bool accType;
+
+public:
+	//___________________________________BankAcc________________________________
+
+	BankAcc(Client& client);
+	virtual ~BankAcc() {
+
+	}
+	void setAccID(string id);
+	void setBalance(double bal);
+	void setAccType(bool type);
+	string getAccID();
+	double getBalance();
+	bool getAccType();
+	Client* get_client();
+	virtual int withdraw(double amount);
+	virtual int deposit(double amount);
+	bool isFoundAccID(string id);
+};
+
+
+
+class SavingBankAcc : public BankAcc
+{
+private:
+	double minimumBalance, minimumWithdraw;
+public:
+	SavingBankAcc(Client& client) : BankAcc(client) {
+		minimumBalance = 1000.0;
+		minimumWithdraw = 100.0;
+	}
+	int withdraw(double amount);
+	int deposit(double amout);
+	virtual ~SavingBankAcc() {
+
+	}
+	int getMiniBalace() {
+		return minimumBalance;
+	}
+	int getMIniWithdraw() {
+		return minimumWithdraw;
+	}
+};
+
+
+class BankApp
+{
+private:
+	vector<BankAcc> bankAcc;
+public:
+	void menu();
+	void addClient();
+	void List();
+	void withdraw();
+	void deposit();
+	BankApp();
+	virtual ~BankApp() {
+
+	}
+};
